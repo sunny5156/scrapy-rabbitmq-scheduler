059ee34dd27bc85ca0db845b079d37ae9d82127b
sunny5156 <sunny5156@qq.com>
2020-06-29 18:03:05 +0800

    edit

--- src/scrapy_rabbitmq_scheduler/spiders.py
+++ src/scrapy_rabbitmq_scheduler/spiders.py
@@ -8,9 +8,9 @@ from scrapy.utils.reqser import request_from_dict
 class RabbitSpider(scrapy.Spider):
     def _make_request(self, mframe, hframe, body):
         try:
-            request = request_from_dict(json.loads(body), self) # json 数据
+            request = request_from_dict(json.loads(body), self) 
         except Exception:
-            body = body.decode()
+            # body = body.decode()
             data = json.loads(body,encoding="utf-8")
             request = scrapy.Request(data['url'], callback=self.parse, dont_filter=True,meta=data['params'])
         return request
